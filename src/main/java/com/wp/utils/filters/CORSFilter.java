b0b19c878c00ac52720fa1d464523db86d42f852
Howard Wang <wangh@it-11663.twpn.root.washpost.com>
2015-06-11 13:39:35 -0400

    Add filters to wp utils

---
+++ src/main/java/com/wp/utils/filters/CORSFilter.java
@@ -0,0 +1,50 @@
+package com.wp.utils.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CORSFilter implements Filter {
+
+  /**
+   * not implemented
+   */
+  public void destroy() {
+  }
+
+  /**
+   * Apply filter to allow cross origin requests
+   *
+   * @throws IOException, ServletException
+   */
+  public void doFilter(ServletRequest req, ServletResponse resp,
+      FilterChain chain) throws IOException, ServletException {
+
+    HttpServletResponse response = (HttpServletResponse) resp;
+
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Methods",
+        "POST, GET, OPTIONS, DELETE");
+    response.setHeader("Access-Control-Max-Age", "3600");
+    response.setHeader("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    response.setHeader("Access-Control-Allow-Credentials", "true");
+
+    chain.doFilter(req, resp);
+  }
+
+  /**
+   * not implemented
+   */
+  public void init(FilterConfig arg0) throws ServletException {
+  }
+
+}
---
+++ src/main/java/com/wp/utils/filters/LoggingFilter.java
@@ -0,0 +1,108 @@
+package com.wp.utils.filters;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Enumeration;
+import java.util.UUID;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import org.apache.commons.io.output.TeeOutputStream;
+import org.json.JSONObject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.mock.web.DelegatingServletOutputStream;
+
+@Component
+
+public final class LoggingFilter extends JSONObject implements Filter {
+
+    private JSONObject headersLog = new JSONObject();
+    private static final Logger headersLogger = LoggerFactory.getLogger("headers");
+    private static final Logger requestLogger = LoggerFactory.getLogger("requests");
+
+    @Override
+    public void init(FilterConfig filterConfigObj) {
+    }
+
+    @Override
+    public void doFilter(
+            ServletRequest request,
+            ServletResponse response,
+            FilterChain chain) throws IOException, ServletException {
+
+        URI uri = null;
+        String referer = "";
+        HttpServletRequest req = (HttpServletRequest) request;
+
+        // Set a unique identifier for the request
+        this.put("type", "request");
+        this.put("uuid", UUID.randomUUID());
+
+        // Basic infos
+        this.put("method", req.getMethod());
+        this.put("path", req.getPathInfo());
+        this.put("uri", req.getRequestURI());
+        this.put("log", req.getMethod() + " " + req.getPathInfo());
+
+        // All the parameters
+        Enumeration<String> params = request.getParameterNames();
+        for (; params.hasMoreElements();) {
+            String param = params.nextElement();
+            this.put("param." + param, request.getParameter(param));
+        }
+
+        // All the headers
+        Enumeration<String> headers = req.getHeaderNames();
+        for (; headers.hasMoreElements();) {
+            String header = headers.nextElement();
+            if (!"cookie".equalsIgnoreCase(header)) {
+                headersLog.put("header." + header, req.getHeader(header));
+            }
+            if (header.equalsIgnoreCase("referer")) {
+                referer = req.getHeader(header);
+                this.put("referer", referer);
+                if ((referer != null) && (!referer.isEmpty())) {
+                    try {
+                        uri = new URI(referer);
+                        this.put("referer-domain", uri.getHost());
+                    } catch (URISyntaxException ex) {
+                        //
+                    }
+                }
+            }
+            if (header.contains("instart-geo")) {
+                this.put("header." + header, req.getHeader(header));
+            }
+        }
+
+        // Run the request
+        OutputStream outstr = new java.io.ByteArrayOutputStream();
+        HttpServletResponse responseWrapper = responseWrapper((HttpServletResponse) response, outstr);
+        chain.doFilter(request, responseWrapper);
+
+        // Log everything
+        headersLogger.info(headersLog.toString());
+        this.put("response", outstr.toString());
+        requestLogger.info(this.toString());
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    private HttpServletResponse responseWrapper(HttpServletResponse response, final OutputStream out) {
+        return new HttpServletResponseWrapper(response) {
+            @Override
+            public ServletOutputStream getOutputStream() throws IOException {
+                return new DelegatingServletOutputStream(
+                        new TeeOutputStream(super.getOutputStream(), out)
+                );
+            }
+        };
+    }
+}
