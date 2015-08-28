eb56457a28e740e4ccd83aadeb367a173d98cd13
nmckoy <nmckoy10@gmail.com>
2015-08-28 12:02:56 -0400

    added new relic notice

--- src/main/java/com/wp/utils/Notifier.java
+++ src/main/java/com/wp/utils/Notifier.java
@@ -1,15 +1,18 @@
 package com.wp.utils;
 
+import com.newrelic.api.agent.NewRelic;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-
+import java.util.HashMap;
+import java.util.Map;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * This class provides a generic notifier to record errors. At the moment we
- * record errors to New Relic. We may add other destinations in the future.
+ * This class provides a generic notifier to record errors.
+ * At the moment we record errors to New Relic.
+ * We may add other destinations in the future.
  *
  * @author Julien Lancien <julien.lancien@washpost.com>
  */
@@ -17,12 +20,13 @@ public class Notifier {
 
     private static final Logger logger = LoggerFactory.getLogger("exceptions");
 
-    // private static final Logger rawlogger =
-    // LoggerFactory.getLogger("exceptions-raw");
-
     public static void noticeError(Exception e) {
+        noticeError(e, null);
+    }
+
+    public static void noticeError(Exception e, HashMap<String, Object> meta) {
         // Don't alert in the middle of the night
-        // NewRelic.noticeError(e);
+        NewRelic.noticeError(e);
 
         StringWriter sw = new StringWriter();
         e.printStackTrace(new PrintWriter(sw));
@@ -32,12 +36,18 @@ public class Notifier {
         log.put("class", e.getClass().getName());
         log.put("type", "exception");
         log.put("log", sw.toString());
+
+        // Add meta information
+        if (meta != null) {
+            for (Map.Entry<String, Object> entry : meta.entrySet()) {
+                String key = entry.getKey();
+                Object val = entry.getValue();
+                log.put(key, val);
+            }
+        }
         logger.info(log.toString());
 
-        // Log to file
-        // rawlogger.error(sw.toString());
     }
 
 }
 
-// vim: set et ts=4 sw=4 sts=4:
